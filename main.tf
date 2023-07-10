5da2f9f06a7a32f1c1692940e6413f8194ed3023
subraban <136383571+subraban@users.noreply.github.com>
2023-07-10 22:52:32 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "google_storage_bucket" "my_bucket" {
+  name     = "sqlnackups"
+  location = "us-central1"
+  storage_class = "STANDARD"
+}
+
+resource "google_storage_bucket_object" "mysql_object1" {
+  name   = var.sqlfile1
+  bucket = google_storage_bucket.my_bucket.name
+  source = var.sql1
+}
+
+resource "google_storage_bucket_object" "mysql_object2" {
+  name   = var.sqlfile2
+  bucket = google_storage_bucket.my_bucket.name
+  source = var.sql2
+}
+resource "google_storage_bucket_object" "mysql_object3" {
+  name   = var.sqlfile3
+  bucket = google_storage_bucket.my_bucket.name
+  source = var.sql3
+}
