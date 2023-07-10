5731ad97f805080a1a789b98e8ff190a152167d3
subraban <136383571+subraban@users.noreply.github.com>
2023-07-10 22:55:30 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "sqlfile1" {
+type=string
+default="DB1.bak"
+}
+variable "sqlfile2" {
+type=string
+default="DB2.bak"
+}
+variable "sqlfile3" {
+type=string
+default="DB3.bak"
+}
+
+variable "sql1" {
+type=string
+default="D:/MSSQL14.SQL2017/DB1.bak"
+}
+variable "sql2" {
+type=string
+default="D:/MSSQL14.SQL2017/DB2.bak"
+}
+variable "sql3" {
+type=string
+default="D:/MSSQL14.SQL2017/DB3.bak"
+}
